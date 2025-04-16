f29f839a391448b71146c067f34583982090e071
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-04-16 21:47:23 +0630

    code update

---
+++ SLHDotNetTrainingBatch1.ConsoleApp/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLHDotNetTrainingBatch1.ConsoleApp
+{
+    internal class Test
+    {
+        public void Test2() { }
+    }
+}
