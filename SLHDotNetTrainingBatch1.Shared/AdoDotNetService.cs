25438a9020c1a2d35d857e053dcfc9964df67ff5
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-05-06 22:52:57 +0630

    feat: add services

---
+++ SLHDotNetTrainingBatch1.Shared/AdoDotNetService.cs
@@ -0,0 +1,79 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace SLHDotNetTrainingBatch1.Shared;
+
+public class AdoDotNetService
+{
+    private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder;
+
+    public AdoDotNetService(SqlConnectionStringBuilder connectionStringBuilder)
+    {
+        _sqlConnectionStringBuilder = connectionStringBuilder;
+    }
+
+    public DataTable Query(string query, List<SqlParameter> parameters)
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddRange(parameters.ToArray());
+        DataTable dt = new DataTable();
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        sqlDataAdapter.Fill(dt);
+
+        connection.Close();
+
+        return dt;
+    }
+
+    public DataTable Query(string query, params SqlParameter[] parameters)
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddRange(parameters);
+        DataTable dt = new DataTable();
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        sqlDataAdapter.Fill(dt);
+
+        connection.Close();
+
+        return dt;
+    }
+
+    public int Execute(string query, params SqlParameter[] parameters)
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddRange(parameters);
+        int result = cmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        return result;
+    }
+
+    public List<T> QueryV2<T>(string query, params SqlParameter[] parameters)
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddRange(parameters);
+        DataTable dt = new DataTable();
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        sqlDataAdapter.Fill(dt);
+
+        connection.Close();
+
+        string jsonStr = JsonConvert.SerializeObject(dt);
+        var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr);
+        return lst!;
+    }
+}
---
+++ SLHDotNetTrainingBatch1.Shared/DapperService.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace SLHDotNetTrainingBatch1.Shared;
+
+public class DapperService
+{
+    private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder;
+
+    public DapperService(SqlConnectionStringBuilder connectionStringBuilder)
+    {
+        _sqlConnectionStringBuilder = connectionStringBuilder;
+    }
+
+    public List<Collin> Query<Collin>(string query, object? parameters = null)
+    {
+        using IDbConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        var lst = connection.Query<Collin>(query, parameters).ToList();
+        return lst;
+    }
+
+    public int Execute(string query, object? parameters = null)
+    {
+        using IDbConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        var result = connection.Execute(query, parameters);
+        return result;
+    }
+}
