12a1a75349d0bafa0fb64834e3b49683d4fee2f1
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-06-01 18:49:20 +0700

    feat: Add model and Service for PlayerPosition

---
+++ SnakeLadderApi/Models/ResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeLadderApi.Models
+{
+    public class ResponseModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+
+    }
+}
