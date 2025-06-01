b4b811f531240abcf5909704612012e093f7398a
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-06-01 18:52:40 +0700

    feat: Add Controller for PlayerPosition

---
+++ SnakeLadderApi/Controllers/PlayerPositionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SnakeLadderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerPositionController : ControllerBase
+    {
+    }
+}
