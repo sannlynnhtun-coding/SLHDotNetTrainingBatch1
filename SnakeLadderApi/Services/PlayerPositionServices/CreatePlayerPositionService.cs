997802c835ccb554b72a6b12583d016adcfee9d7
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-06-02 19:29:47 +0700

    feat: Add Crete Player Position Api

---
+++ SnakeLadderApi/Services/PlayerPositionServices/CreatePlayerPositionService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using SnakeLadder.Database.Entities;
+using SnakeLadderApi.Models;
+using SnakeLadderApi.Models.PlayerPosition;
+
+namespace SnakeLadderApi.Services.PlayerPositionServices;
+
+public class CreatePlayerPositionService
+{
+
+    private readonly AppDbContext _context;
+
+    public CreatePlayerPositionService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public class DiceRoller
+    {
+        private static Random random = new Random();
+
+        public static int RollDie()
+        {
+            return random.Next(1, 7); // 7 is exclusive, so this returns 1 to 6
+        }
+    }
+
+    public CreatePlayerPositionResponseModel CreatePlayerPosition(UpdatePlayerPositionRequestModel requestModel)
+    {
+        /*
+         - Select previous RowNo by PlayerId
+         - Take a number ramdonly within 1 to 6
+         - Sum ramdon number and previous RowNo 
+         - Check current updatedRowNo is same or not with Helper's start number and calculate with it
+         - Insert record
+         */
+
+        CreatePlayerPositionResponseModel model;
+        int randomNo = DiceRoller.RollDie();
+        var currentPositionNo = 0;
+        int updatedPositionNo = currentPositionNo + randomNo;
+
+        var item = _context.TblPlayerPositions.Where(x => x.PlayerId == requestModel.PlayerId)
+                    .OrderByDescending(x => x.CreatedDate)
+                    .Take(1)
+                    .SingleOrDefault()!;
+        if (item is not null)
+        {
+             currentPositionNo = item.CurrentPosition;
+        }
+        updatedPositionNo = currentPositionNo + randomNo;
+
+        var itemSnake = _context.TblSnakes.FirstOrDefault(x => x.FromPosition == updatedPositionNo)!;
+        if (itemSnake is not null)
+        {
+            updatedPositionNo = itemSnake.ToPosition;
+        }
+
+        var itemLadder = _context.TblLadders.FirstOrDefault(x => x.FromPosition == updatedPositionNo)!;
+        if (itemLadder is not null)
+        {
+            updatedPositionNo = itemLadder.ToPosition;
+        }
+
+        _context.TblPlayerPositions.Add(new TblPlayerPosition
+        {
+            PlayerId = requestModel.PlayerId,
+            CurrentPosition = updatedPositionNo,
+            CreatedDate = DateTime.UtcNow,
+        });
+        _context.SaveChanges();
+
+         return model = new CreatePlayerPositionResponseModel
+        {
+            PlayerId = requestModel.PlayerId,
+            CurrentPosition = updatedPositionNo,
+            CreatedDate = DateTime.UtcNow,
+        };
+
+
+    }
+}
+
