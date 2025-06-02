64b59e10ca4556caae00d07644e19cf5a2cd6e4e
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-06-02 21:43:05 +0630

    feat: RenderSectionAsync, PartialView

---
+++ SLHDotNetTrainingBatch1.MvcApp2/Controllers/WalletController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace SLHDotNetTrainingBatch1.MvcApp2.Controllers
+{
+    public class WalletController : Controller
+    {
+        private readonly SqlConnectionStringBuilder sqlConnectionStringBuilder;
+
+        public WalletController(IConfiguration configuration)
+        {
+            sqlConnectionStringBuilder = new SqlConnectionStringBuilder(configuration.GetConnectionString("DbConnection"));
+        }
+
+        [ActionName("Index")]
+        public IActionResult WalletIndex()
+        {
+            return View("WalletIndex");
+        }
+    }
+}
