d6129da583e51540c59ef6dcb448d8d6b1ec3d02
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-06-03 22:04:15 +0630

    feat: add createWallet.js

---
+++ SLHDotNetTrainingBatch1.MvcApp3/wwwroot/js/wallet/createWallet.js
@@ -0,0 +1,30 @@
+﻿$('#btnSave').click(function () {
+    let item = {
+        WalletUserName: $('#txtWalletUserName').val(),
+        FullName: $('#txtFullName').val(),
+        MobileNo: $('#txtMobileNo').val(),
+        Balance: $('#txtBalance').val()
+    };
+
+    $.ajax({
+        url: "/Wallet/Save",
+        type: "POST",
+        data: { requestModel: item },
+        success: function (response) {
+            console.log({ response });
+            if (!response.IsSuccess) {
+                alert("Error: " + response.Message);
+                return;
+            }
+
+            alert(response.Message);
+            window.location.href = "/Wallet/Index";
+
+            //$("#createModal").modal("hide");
+            //loadData();
+        },
+        error: function (request, status, error) {
+            alert(request.responseText);
+        }
+    });
+})
