9dc9a882be341838e218d7a47065bad4d0597e5a
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-06-17 22:25:33 +0630

    feat: Real-time Chat App

---
+++ SLHDotNetTrainingBatch1.SignalrExample/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SLHDotNetTrainingBatch1.SignalrExample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // server event / listener
+        // send message
+        public async Task ServerReceiveMessage(string user, string message)
+        {
+            // ReceiveMessage => event
+            await Clients.AllExcept(Context.ConnectionId).SendAsync("ClientReceiveMessage", user, message);
+        }
+    }
+}
