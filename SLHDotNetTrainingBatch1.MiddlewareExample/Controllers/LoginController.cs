c15245b1c4700c9baca7ad3f2b2899cc582a081d
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-06-26 21:03:28 +0630

    feat: middleware

---
+++ SLHDotNetTrainingBatch1.MiddlewareExample/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace SLHDotNetTrainingBatch1.MiddlewareExample.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginRequestModel requestModel)
+        {
+            // db => login table data == request
+
+            //HttpContext.Response.Cookies.Add("Username", requestModel.Username);    
+            var opts = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = DateTime.Now.AddMinutes(30),
+                SameSite = SameSiteMode.Lax,
+                Secure = true
+            };
+            //HttpContext.Response.Cookies.Delete("Username");
+            HttpContext.Response.Cookies.Append("Username", requestModel.Username, opts);
+
+            return Redirect("/Home");
+        }
+    }
+
+    public class LoginRequestModel
+    {
+        public string Username { get; set;}
+        public string Password { get; set;}
+    }
+}
---
+++ SLHDotNetTrainingBatch1.MiddlewareExample/Middlewares/CookieMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace SLHDotNetTrainingBatch1.MiddlewareExample.Middlewares
+{
+    public class CookieMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CookieMiddleware(RequestDelegate next)
+        {
+            _next=next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string username = context.Request.Cookies["Username"]!;
+            if (!allowedUrls.Any(x => x == context.Request.Path))
+            {
+                if (string.IsNullOrEmpty(username))
+                {
+                    context.Response.Redirect("/Login");
+                }
+            }
+
+            await _next(context);
+        }
+
+        private string[] allowedUrls = { "/", "/login", "/login/index" };
+    }
+}
